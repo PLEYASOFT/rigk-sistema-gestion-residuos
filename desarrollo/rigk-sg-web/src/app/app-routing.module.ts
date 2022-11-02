dd7683757f124cbef4b05c7769e35d51182545f7
Patricio Angelini <pangelini@kali>
2022-11-02 15:40:46 -0300

    front arq

--- desarrollo/rigk-sg-web/src/app/app-routing.module.ts
+++ desarrollo/rigk-sg-web/src/app/app-routing.module.ts
@@ -1,28 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { CheckRolGuard } from './core/guards/check-rol.guard';
 
-import { HomeComponent } from './modules/home/pages/home/home.component';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent,
-    canActivate: [CheckRolGuard],
-    data: {
-      expectedRol: ['Operador CCA', 'Administrador CCA'],
-    },
-  },
-  {
-    path: '*',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- desarrollo/rigk-sg-web/src/app/app.module.ts
+++ desarrollo/rigk-sg-web/src/app/app.module.ts
@@ -1,25 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from './shared/shared.module';
-import { LoginModule } from './modules/login/login.module';
-import { HomeModule } from './modules/home/home.module';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    AppRoutingModule,
-    RouterModule,
     BrowserModule,
-    HttpClientModule,
-    SharedModule,
-    LoginModule,
-    HomeModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
