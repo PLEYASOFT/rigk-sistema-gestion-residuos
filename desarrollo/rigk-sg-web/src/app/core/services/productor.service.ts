cab9a86e4a2fc779979b28339d3429583d7e9266
Claudio Vega Lagos <claudio.vegal@sansano.usm.cl>
2022-11-05 20:48:08 -0300

    ejemplo login

--- desarrollo/rigk-sg-web/src/app/core/services/productor.service.ts
+++ desarrollo/rigk-sg-web/src/app/core/services/productor.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductorService {
 
-  constructor() { }
+  url = `${environment.API_V1_URL}/auth`;
+
+  constructor(private http: HttpClient) { }
+
+  login(user:string,password:string) {
+
+    // const tmp = {
+    //   status: true,
+    //   data: [... ""]
+    // }
+
+    return this.http.post<any>(`${this.url}/login`, {user,password});
+  }
+
 }
--- desarrollo/rigk-sg-web/src/app/modules/auth/pages/login/login.component.ts
+++ desarrollo/rigk-sg-web/src/app/modules/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductorService } from '../../../../core/services/productor.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authSerice: ProductorService) { }
 
   ngOnInit(): void {
+  }
+
+  btnLogin() {
+    const usuario = "lalla";
+    const contrasenia = "bbb";
+
+
+    this.authSerice.login(usuario, contrasenia).subscribe(r=>{
+      if(r.status) {
+        // ... ok logeado
+      } else {
+        // no login
+      }
+    });
   }
 
 }
