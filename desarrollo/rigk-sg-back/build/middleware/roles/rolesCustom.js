e43bd69aa92131cf46582dbe5afe0826b1eac8f7
Claudio Vega Lagos <claudio.vegal@sansano.usm.cl>
2022-11-08 16:55:06 -0300

    delete trash

--- desarrollo/rigk-sg-back/build/middleware/roles/rolesCustom.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.verifyRolProductor = void 0;
-const VerifyProductor_1 = require("./VerifyProductor");
-Object.defineProperty(exports, "verifyRolProductor", { enumerable: true, get: function () { return VerifyProductor_1.verifyRolProductor; } });
