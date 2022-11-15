bb9728a603b63146cb7cda0910def48650ea440e
kathy-23 <katherines.labarca@gmail.com>
2022-11-15 11:22:09 -0300

    [change]feature/update-password

--- desarrollo/rigk-sg-web/src/app/modules/productor-statement/pages/profile/profile.component.ts
+++ desarrollo/rigk-sg-web/src/app/modules/productor-statement/pages/profile/profile.component.ts
@@ -27,9 +27,9 @@ export class ProfileComponent implements OnInit {
     this.authService.modifyPassword(password, repeatPassword, actual).subscribe(resp => {
     if (resp.status){
       Swal.fire({
-        title:"cambio de contraseña",
-        text:"la contraseña fue cambiada exitosamente",
-        icon:"success"
+        title:"Cambio de contraseña",
+        text:"La contraseña fue cambiada exitosamente",
+        icon:"success",
       })
       this.router.navigate(['/auth/login']);
     }
