69da5ce4e82b0e25807acb3f7c47e0579701999c
Claudio Vega Lagos <claudio.vegal@sansano.usm.cl>
2023-01-04 08:05:23 -0300

    fix filtro

--- desarrollo/rigk-sg-web/src/app/modules/productor-statement/pages/statements/statements.component.ts
+++ desarrollo/rigk-sg-web/src/app/modules/productor-statement/pages/statements/statements.component.ts
@@ -10,72 +10,79 @@ export class StatementsComponent implements OnInit {
 
   dbStatements: any[] = [];
   db: any[] = [];
-  pos=1;
+  pos = 1;
 
-  business_name:any[] = [];
+  business_name: any[] = [];
   years: number[] = [];
 
   constructor(public productorService: ProductorService) { }
 
   ngOnInit(): void {
     this.loadStatements();
-    
+
   }
 
   loadStatements() {
-    this.productorService.getStatementByUser.subscribe(r=>{
+    this.productorService.getStatementByUser.subscribe(r => {
       console.log(r);
-      if(r.status) {
-        (r.data as any[]).forEach(e=>{
+      if (r.status) {
+        (r.data as any[]).forEach(e => {
           console.log(e);
-          if(this.business_name.indexOf(e.NAME_BUSINESS) == -1) {
+          if (this.business_name.indexOf(e.NAME_BUSINESS) == -1) {
             this.business_name.push(e.NAME_BUSINESS);
           }
-          if(this.years.indexOf(e.YEAR_STATEMENT) == -1){
+          if (this.years.indexOf(e.YEAR_STATEMENT) == -1) {
             this.years.push(e.YEAR_STATEMENT)
-          } 
+          }
         });
-        console.log("aa",this.business_name);
+        console.log("aa", this.business_name);
         this.dbStatements = r.data;
-        this.db =  this.dbStatements.slice(0,10);
+        this.db = this.dbStatements.slice(0, 10);
       }
     })
   }
   filter() {
     const n = (document.getElementById('f_name') as HTMLSelectElement).value;
     const y = (document.getElementById('f_year') as HTMLSelectElement).value;
-    this.db = this.dbStatements.map(r=>{
-      if(n != '' && r.NAME_BUSINESS == n) {
-        if(y != '' && r.YEAR_STATEMENT == y) {
+    this.db = this.dbStatements.filter(r => {
+      if (n != '-1' && r.NAME_BUSINESS == n) {
+        if (y != '-1') {
+          if (r.YEAR_STATEMENT == y)
             return r;
+        } else {
+          return r;
         }
-        return r;
       }
-      if(y != '') {
-        r
+      if (n == '-1') {
+        if (y != '-1') {
+          if (r.YEAR_STATEMENT == y)
+            return r;
+        } else {
+          return r;
+        }
       }
-    }).slice(0,10);
-    
+    }).slice(0, 10);
+
   }
-  reset(){
+  reset() {
     this.loadStatements();
   }
 
   setArrayFromNumber(i: number) {
-    this.db =  this.dbStatements.slice((i*10)-1,(i+1)*10);
+    this.db = this.dbStatements.slice((i * 10) - 1, (i + 1) * 10);
   }
   next() {
-    if((this.pos)*10 > this.dbStatements.length) return;
+    if ((this.pos) * 10 > this.dbStatements.length) return;
     this.pos++;
     console.log(this.pos);
-    this.db =  this.dbStatements.slice((this.pos-1)*10, (this.pos)*10);
+    this.db = this.dbStatements.slice((this.pos - 1) * 10, (this.pos) * 10);
   }
   previus() {
-    if(this.pos-1 == 0) return;
+    if (this.pos - 1 == 0) return;
     this.pos--;
-    this.db =  this.dbStatements.slice((this.pos-1)*10,(this.pos)*10);
+    this.db = this.dbStatements.slice((this.pos - 1) * 10, (this.pos) * 10);
   }
-  downloadPDF(id: any, year:any) {
-    this.productorService.downloadPDF(id,year);
+  downloadPDF(id: any, year: any) {
+    this.productorService.downloadPDF(id, year);
   }
 }
