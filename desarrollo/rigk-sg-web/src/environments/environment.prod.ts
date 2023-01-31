e82c9753ba1397768943d4c69c52bd51f387d2b8
Patricio Angelini <pangelini@kali>
2023-01-31 11:26:25 -0300

    foo

--- desarrollo/rigk-sg-web/src/environments/environment.prod.ts
+++ desarrollo/rigk-sg-web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_V1_URL: 'http://prorep-back-env.eba-yuxkxdzq.us-east-1.elasticbeanstalk.com/api/v1'
-  //API_V1_URL: 'https://pleyades.dynu.net/api/v1'
+  //API_V1_URL: 'http://prorep-back-env.eba-yuxkxdzq.us-east-1.elasticbeanstalk.com/api/v1'
+  API_V1_URL: 'https://pleyades.dynu.net/api/v1'
 };
