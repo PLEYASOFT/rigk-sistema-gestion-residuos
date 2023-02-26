d71e35852595b7c433b0df73ce6f2beab61e5a83
Claudio Vega Lagos <claudio.vegal@sansano.usm.cl>
2023-02-26 10:19:37 -0300

    vulnerabilidades pentests

--- desarrollo/rigk-sg-back/server.ts
+++ desarrollo/rigk-sg-back/server.ts
@@ -1,8 +1,8 @@
-import express, { Application } from 'express';
+import express, { Application, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import fileUpload from 'express-fileupload';
-
+import helmet from 'helmet';
 import loginRoutes from './routes/login';
 import businessRoutes from './routes/business';
 import statementRoutes from './routes/statmentProductor';
@@ -10,7 +10,6 @@ import ratesRoutes from './routes/rates';
 import establishmentRoutes from './routes/establishment';
 import consumerRoutes from './routes/consumer';
 class Server {
-
     private app: Application;
     private port: string;
     private apiPath = {
@@ -19,50 +18,52 @@ class Server {
         form: '/api/v1/statement',
         consumer: '/api/v1/consumer',
         rates: '/api/v1/rates',
-        establishment:'/api/v1/establishment'
+        establishment: '/api/v1/establishment'
     }
-
     constructor() {
-        this.app  = express();
+        this.app = express();
         this.port = process.env.PORT_SV || '3000';
         this.config();
-        
         this.routes();
     }
     routes() {
-        this.app.use(this.apiPath.auth,loginRoutes);
-        this.app.use(this.apiPath.business,businessRoutes);
+        this.app.use(this.apiPath.auth, loginRoutes);
+        this.app.use(this.apiPath.business, businessRoutes);
         this.app.use(this.apiPath.consumer, consumerRoutes);
-        this.app.use(this.apiPath.form,statementRoutes);
-        this.app.use(this.apiPath.rates,ratesRoutes);
-        this.app.use(this.apiPath.establishment,establishmentRoutes)
-        this.app.use((error:any, req:any, res:any, next:any) => {
+        this.app.use(this.apiPath.form, statementRoutes);
+        this.app.use(this.apiPath.rates, ratesRoutes);
+        this.app.use(this.apiPath.establishment, establishmentRoutes);
+        this.app.use((error: any, req: any, res: any, next: any) => {
             if (error) {
                 console.log(error)
-                res.status(400).json({status:false, msg:"Error general"});
+                res.status(400).json({ status: false, msg: "Error general" });
                 next();
-              } else {
+            } else {
                 next(error);
-              }
-        })
+            }
+        });
     }
-
     config() {
-        this.app.use(express.urlencoded({extended: false}));
-        this.app.use(express.json({strict: true}));
-        this.app.use(cors({origin: '*'}));
+        this.app.use(helmet({
+            contentSecurityPolicy: false,
+            hidePoweredBy: true,
+            frameguard: { action: 'deny' },
+            crossOriginResourcePolicy: false,
+            crossOriginEmbedderPolicy: false,
+            xssFilter: true
+        }));
+        this.app.use(helmet.xssFilter());
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(express.json({ strict: true }));
+        this.app.use(cors({ origin: '*' }));
         this.app.use(fileUpload())
         this.app.use(morgan('dev'));
-        
-    }
 
+    }
     listen() {
-        this.app.listen( this.port, () => {
+        this.app.listen(this.port, () => {
             console.log(`Servidor escuchando en puerto ${this.port}`);
         });
     }
-
-    
 }
-
 export default Server;
