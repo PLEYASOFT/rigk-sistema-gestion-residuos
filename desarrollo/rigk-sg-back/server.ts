e16099a317ebb3019214fc23d9667962592bd97e
Juan Arredondo <juan.arredondo.c@usach.cl>
2023-03-21 17:04:39 -0300

    fix

--- desarrollo/rigk-sg-back/server.ts
+++ desarrollo/rigk-sg-back/server.ts
@@ -46,7 +46,7 @@ class Server {
     }
     config() {
         const opts = {
-            points: 100,
+            points: 20,
             duration: 1,
             blockDuration: 300
         };
