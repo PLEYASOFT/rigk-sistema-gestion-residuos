f438ac483a66c4003af694f0da26c61350ee0b27
Juan Arredondo <juan.arredondo.c@usach.cl>
2023-05-08 16:55:54 -0400

    fix incidencias 1,2,3

--- desarrollo/rigk-sg-back/dao/authDao.ts
+++ desarrollo/rigk-sg-back/dao/authDao.ts
@@ -16,19 +16,23 @@ class AuthDao {
         const conn = mysqlcon.getConnection()!;
         const res: any = await conn.query("SELECT USER.*,USER_ROL.ROL_ID AS ROL, ROL.NAME AS ROL_NAME, BUSINESS.NAME AS BUSINESS, BUSINESS.ID as ID_BUSINESS FROM USER INNER JOIN USER_ROL ON USER_ROL.USER_ID = USER.ID INNER JOIN USER_BUSINESS ON USER_BUSINESS.ID_USER = USER.ID INNER JOIN BUSINESS ON BUSINESS.ID = USER_BUSINESS.ID_BUSINESS INNER JOIN ROL ON ROL.ID=USER_ROL.ROL_ID WHERE USER.EMAIL = ? AND user.STATE=1", [USER]).then((res) => res[0]).catch(error => [{ undefined }]);
         let user: any = {}
-        let name_business = []
+        let name_business = [];
+        let id_business = [];
         for (let i = 0; i < res.length; i++) {
             name_business.push(res[i].BUSINESS);
+            id_business.push(res[i].ID_BUSINESS);
         }
-        user = { ...res[0] }
+        user = { ...res[0] };
         user.BUSINESS = name_business;
+        user.ID_BUSINESS = id_business;
         let login = false;
         if (res != null && res != undefined && res.length > 0) {
             login = true;
         }
         conn.end();
-        return res[0] || undefined;
+        return user || undefined;
     }
+    
     async verifyEmail(USER: string) {
         const conn = mysqlcon.getConnection()!;
         const res: any = await conn.query("SELECT EMAIL,ID,PASSWORD FROM USER WHERE EMAIL = ?", [USER]).then((res) => res[0]).catch(error => [{ undefined }]);
