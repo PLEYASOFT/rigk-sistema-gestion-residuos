9d7d7e238adb313725af2db28d22a26c7af5fcd0
Claudio Vega Lagos <claudio.vegal@sansano.usm.cl>
2023-05-24 11:10:36 -0400

    foo

--- desarrollo/rigk-sg-back/helpers/sendOC.ts
+++ desarrollo/rigk-sg-back/helpers/sendOC.ts
@@ -34,7 +34,6 @@ export const sendOC = async ( id: any, file: any ) => {
             console.error('Ha ocurrido un error:',err);
             return false;
         }
-        console.log(response);
         return true;
     });
 }
