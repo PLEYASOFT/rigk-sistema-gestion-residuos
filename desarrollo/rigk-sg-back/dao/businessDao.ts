69e6b643cb013272181ff63a316cbe8b14069faf
Katherine Labarca <62617863+kathy-23@users.noreply.github.com>
2023-06-12 12:45:25 -0400

    [IMP] Declaraciones pagina principal

--- desarrollo/rigk-sg-back/dao/businessDao.ts
+++ desarrollo/rigk-sg-back/dao/businessDao.ts
@@ -80,10 +80,8 @@ class BusinessDao {
     public async getAllBusinessById(id: string, year: string) {
         const conn = mysqlcon.getConnection()!;
         const business: any = await conn.query("SELECT business.* FROM user_business INNER JOIN business on business.ID = user_business.ID_BUSINESS WHERE user_business.ID_USER=? GROUP BY user_business.ID_BUSINESS", [id]).then(res => res[0]).catch(erro => {
-            console.log(erro);
             return undefined
         });
-        console.log("",business);
         const statements: any = await conn.query("SELECT header_statement_form.STATE, header_statement_form.ID_BUSINESS FROM header_statement_form inner join user_business ON user_business.ID_BUSINESS = header_statement_form.ID_BUSINESS  WHERE user_business.ID_USER=? AND header_statement_form.YEAR_STATEMENT=? GROUP BY user_business.ID_BUSINESS", [id, year]).then(res => res[0]).catch(erro => undefined);
         if (business == null || business.length == 0) {
             return false;
