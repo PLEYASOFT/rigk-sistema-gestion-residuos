9cfd4334be7cf60f36ca688ebd0f07fe437e56a1
Katherine Labarca <62617863+kathy-23@users.noreply.github.com>
2023-07-26 12:52:33 -0400

    [IMP]Maintainer rates component

--- desarrollo/rigk-sg-back/routes/rates.ts
+++ desarrollo/rigk-sg-back/routes/rates.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import maintainerRatesLogic from "../controllers/maintainerRatesLogic";
 import ratesLogic from "../controllers/ratesLogic"
 import { validarJWT } from "../middleware/validar-jwt";
 const router = Router();
 router.get('/uf', [validarJWT], ratesLogic.getUfDay);
 router.get('/uf/:date', [validarJWT], ratesLogic.getUfDate);
 router.get('/clp', [validarJWT], ratesLogic.getRatesCLP);
+router.get('/algo', [validarJWT], ratesLogic.getAllRates);
 router.get('/:year', [validarJWT], ratesLogic.getRatesByYear);
+router.post('/', [validarJWT], ratesLogic.saveRates);
+router.put('/updateRatesYear', [validarJWT], ratesLogic.updateRates);
+router.put('/rates/:id', [validarJWT], ratesLogic.updateRates);
 export default router;
