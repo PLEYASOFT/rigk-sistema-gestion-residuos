e984e633125e35b456bc52f09d8d09bdf7ab0716
tomas-abarca <tomas.munoz@pleyasoft.com>
2023-08-21 18:32:57 -0400

    Formato de fechas

--- desarrollo/rigk-sg-back/controllers/managerLogic.ts
+++ desarrollo/rigk-sg-back/controllers/managerLogic.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import managerDao from '../dao/managerDao';
 import { createLog } from "../helpers/createLog";
 import ExcelJS from 'exceljs';
+import establishmentDao from "../dao/establishmentDao";
+import { string } from "joi";
 class ManagerLogic {
     async addManager(req: Request|any, res: Response) {
         const type_material = req.body.type_material;
@@ -87,7 +89,15 @@ class ManagerLogic {
         // const { id } = req.params;
         try {
             const path = require('path');
-            const outputPath = path.join(__dirname, `../../files/templates/_carga_masiva_y.xlsx`);
+            const outputPath = path.join(__dirname, `../../files/templates/_carga_masiva_z.xlsx`);
+            const invoices = await establishmentDao.getDeclarationEstablishment(req.uid);
+            if(invoices == false ){
+                return res.status(500).json({
+                    status: false,
+                    message: "No existen facturas pendientes"
+                });
+            }
+
             /**
              * DATA VALIDATION
              */
@@ -98,19 +108,87 @@ class ManagerLogic {
             //  */
             const worksheet = workbook.addWorksheet('Carga Masiva');
             const row = worksheet.getRow(1);
-            row.getCell(1).value = "CODIGO ESTABLECIMIENTO";
-            row.getCell(2).value = "FECHA DE RETIRO";
-            row.getCell(3).value = "NUM GUIA DESPACHO";
-            row.getCell(4).value = "TIPO TRATAMIENTO";
-            row.getCell(5).value = "TIPO RESIDUO";
-            row.getCell(6).value = "TIPO ESPECIFICO";
-            row.getCell(7).value = "CÓDIGO LER";
-            row.getCell(8).value = "NOMBRE GESTOR";
-            row.getCell(9).value = "RUT GESTOR";
-            row.getCell(10).value = "CÓDIGO ESTABLECIMIENTO RECEPTOR";
-            row.getCell(11).value = "CÓDIGO TRATAMIENTO RECEPTOR";
-            row.getCell(12).value = "CANTIDAD (KG)";
+            row.getCell(1).value = "EMPRESA CI";
+            row.getCell(2).value = "ESTABLECIMIENTO";
+            row.getCell(3).value = "TIPO TRATAMIENTO";
+            row.getCell(4).value = "MATERIAL";
+            row.getCell(5).value = "SUBTIPO";
+            row.getCell(6).value = "FECHA RETIRO";
+            row.getCell(7).value = "NÚM. FACTURA RECICLADOR";
+            row.getCell(8).value = "RUT RECICLADOR";
+            row.getCell(9).value = "RECICLADOR";
+            row.getCell(10).value = "FECHA INGRESO PR";
+            row.getCell(11).value = "PESO TOTAL";
+            row.getCell(12).value = "PESO DECLARADO";
+            row.getCell(13).value = "PESO VALORIZADO";
             row.commit();
+
+
+            const col = worksheet.columns;
+            col[0].width = 23,43;  //A
+            col[1].width = 36;     //B
+            col[2].width = 21,14;  //C
+            col[3].width = 12;  //D
+            col[4].width = 33,71;  //E
+            col[5].width = 12,71;  //F
+            col[6].width = 25,71;  //G
+            col[7].width = 15,14;  //H
+            col[8].width = 22,86;  //I
+            col[9].width = 17,14;  //J
+            col[10].width = 10,86; //K
+            col[11].width = 17;    //L
+            col[12].width = 17;    //M
+
+            for (let i = 0; i < invoices.length; i++) {
+                const invoice = invoices[i];
+                // if (establishment.VALUE === 0) {
+                    const dateFormat = new Date(invoice.FechaRetiro);
+                    
+                    let day = dateFormat.getDate();
+                    let month = dateFormat.getMonth();
+                    let year = dateFormat.getFullYear();
+
+                    let monthWithTwoDigits = month.toString().padStart(2, '0');
+                    let dayWithTwoDigits = day.toString().padStart(2, '0');
+                    let format1 = dayWithTwoDigits + "/" + monthWithTwoDigits + "/" + year;
+              
+                    const rowdata = worksheet.getRow(i + 2);
+                    rowdata.getCell(1).value = `${invoice.NAME_BUSINESS}`;
+                    rowdata.getCell(2).value = `${invoice.NAME_ESTABLISHMENT_REGION}`;
+                    rowdata.getCell(3).value = `${invoice.TipoTratamiento}`;
+                    rowdata.getCell(4).value = `${invoice.PRECEDENCE}`;
+                    rowdata.getCell(5).value = `${invoice.TYPE_RESIDUE}`;
+                    rowdata.getCell(6).value = `${format1}`;
+                    rowdata.getCell(7).value = "";
+                    rowdata.getCell(8).value = "";
+                    rowdata.getCell(9).value = "";
+                    rowdata.getCell(10).value = "";
+                    rowdata.getCell(11).value = "";
+                    rowdata.getCell(12).value = `${invoice.VALUE}`;
+                    rowdata.getCell(13).value = "";
+                    rowdata.commit();
+                // }
+            }
+            
+            const maxRows = invoices.length; 
+            for (let i = 1; i <= maxRows; i++) {
+
+                
+                //if (worksheet.getCell(`J${i + 1}`).value != "") {
+                
+                worksheet.getCell(`J${i + 1}`).dataValidation = {
+                    type: 'textLength',
+                    allowBlank: false,
+                    operator: 'between',
+                    showErrorMessage: true,
+                    errorStyle: 'error',
+                    errorTitle: 'Formato de Fecha Inválido',
+                    error: 'Por favor, ingrese una fecha válida en formato DD/MM/AAAA.',
+                    formulae: [10, 10]
+                };
+                worksheet.getCell(`J${i + 1}`).numFmt = '@';;
+            //}
+            }
 
             await workbook.xlsx.writeFile(outputPath);
             return res.download(outputPath);
