4abaa714600fb338fb85ffa416e8e48380608435
Felipe-Tello <felipei9ta@gmail.com>
2023-10-10 12:19:45 -0300

    Excel y somke test listos

--- desarrollo/rigk-sg-back/dao/establishmentDao.ts
+++ desarrollo/rigk-sg-back/dao/establishmentDao.ts
@@ -39,7 +39,7 @@ class EstablishmentDao {
         const conn = mysqlcon.getConnection()!;
         await conn.execute("SET lc_time_names = 'es_ES';");
         const data: any = await conn.execute(`
-            SELECT CONCAT(establishment.NAME_ESTABLISHMENT, ' - ', establishment.REGION) AS NAME_ESTABLISHMENT_REGION,
+            SELECT CONCAT(establishment.NAME_ESTABLISHMENT, ' - ' , communes.NAME, ' - ', establishment.REGION) AS NAME_ESTABLISHMENT_REGION,
                 header_industrial_consumer_form.CREATED_AT, header_industrial_consumer_form.YEAR_STATEMENT,
                 header_industrial_consumer_form.ID AS ID_HEADER, business.NAME as NAME_BUSINESS, detail_industrial_consumer_form.ID AS ID_DETAIL,
                 CASE
@@ -67,6 +67,7 @@ class EstablishmentDao {
         INNER JOIN establishment_business ON establishment_business.ID_ESTABLISHMENT = establishment.ID
         INNER JOIN business ON business.ID = establishment_business.ID_BUSINESS
         INNER JOIN detail_industrial_consumer_form ON detail_industrial_consumer_form.ID_HEADER = header_industrial_consumer_form.ID
+        INNER JOIN communes ON communes.ID = establishment.ID_COMUNA
         LEFT JOIN invoices_detail ON invoices_detail.ID_DETAIL = detail_industrial_consumer_form.ID
         LEFT JOIN type_material ON type_material.ID = detail_industrial_consumer_form.PRECEDENCE
         LEFT JOIN submaterial ON submaterial.ID = detail_industrial_consumer_form.TYPE_RESIDUE
@@ -95,13 +96,13 @@ class EstablishmentDao {
         return establishment;
     }
     
-    public async getInvoice(number: any, rut: any, treatment_type: number, material_type: number, id_business: number) {
+    public async getInvoice(number: any, rut: any, treatment_type: number, material_type: number) {
         const conn = mysqlcon.getConnection()!;
         const business: any = await conn.execute("SELECT ID, NAME FROM business WHERE VAT = ?", [rut]).then((res) => res[0]).catch(error => { console.log(error); return [{ undefined }] });
         if (business == null || business.length == 0) {
             return []
         }
-        const data0:any = await conn.execute("SELECT ID, VALUED_TOTAL AS invoice_value,TREATMENT_TYPE,MATERIAL_TYPE FROM invoices WHERE INVOICE_NUMBER=? AND VAT=? AND ID_BUSINESS=?", [number, rut, id_business]).then((res) => res[0]).catch(error => [{ undefined }]);
+        const data0:any = await conn.execute("SELECT ID, VALUED_TOTAL AS invoice_value,TREATMENT_TYPE,MATERIAL_TYPE FROM invoices WHERE INVOICE_NUMBER=? AND VAT=?", [number, rut]).then((res) => res[0]).catch(error => [{ undefined }]);
         // first invoice
         if (data0 == null || data0.length == 0) {
             return [{
