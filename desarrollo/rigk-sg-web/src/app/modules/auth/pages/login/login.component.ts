6445926593fa224953cb91fae99d1c32b1ec182f
Juan Arredondo <juan.arredondo.c@usach.cl>
2023-10-18 11:14:14 -0300

    fix

--- desarrollo/rigk-sg-web/src/app/modules/auth/pages/login/login.component.ts
+++ desarrollo/rigk-sg-web/src/app/modules/auth/pages/login/login.component.ts
@@ -21,11 +21,12 @@ export class LoginComponent implements OnInit {
     user: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(8)]]
   });
+  data: any;
 
   constructor(private fb: FormBuilder,
-              private authService: AuthService,
-              private router: Router,
-              private actived: ActivatedRoute) {
+    private authService: AuthService,
+    private router: Router,
+    private actived: ActivatedRoute) {
     this.actived.queryParams.subscribe(r => {
       if (r['logout']) {
         sessionStorage.clear();
@@ -41,35 +42,37 @@ export class LoginComponent implements OnInit {
 
   btnLogin() {
     const { user, password } = this.formData.value;
-    this.authService.login(user,password).subscribe({
-      next: resp=> {
+    this.authService.login(user, password).subscribe({
+      next: resp => {
         if (!resp.body.status) {
           this.error = true;
           this.msg = resp.body.msg;
         } else {
           const horaIngreso = new Date();
           sessionStorage.setItem('user', JSON.stringify(resp.body.data.user));
-          sessionStorage.setItem('horaIngreso',horaIngreso.toString());
+          sessionStorage.setItem('horaIngreso', horaIngreso.toString());
+          
           if (resp.body.data.user.ROLES.length == 1) {
-            const rol = resp.body.data.user.ROLES[0];
+            const rol = resp.body.data.user.ROLES[0].id;
             const userObj = JSON.parse(sessionStorage.getItem('user')!);
             userObj.ROL = rol;
             sessionStorage.setItem('user', JSON.stringify(userObj));
-            if(rol == 9){
+            if (rol == 9) {
               this.router.navigate(['/productor']);
             }
-            else if(rol == 10){
+            else if (rol == 10) {
               this.router.navigate(['/mantenedor']);
             }
-            else if(rol == 11){
+            else if (rol == 11) {
               this.router.navigate(['/consumidor']);
             }
-            else if(rol == 12){
+            else if (rol == 12) {
               this.router.navigate(['/gestor']);
             }
-          }else{
+          } else {
             this.multiRol = true;
             this.userRoles = resp.body.data.user.ROLES;
+            this.data = resp.body.data.user;
           }
         }
       },
@@ -89,15 +92,38 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  navigateToRole(rol: string) {
-    if (rol === 'Admin') {
-      this.router.navigate(['/admin']);
-    } else if (rol === 'Gestor') {
-      this.router.navigate(['/gestor']);
-    } else if (rol === 'Consumidor') {
-      this.router.navigate(['/consumidor']);
-    } else if (rol === 'Productor') {
+  navigateToRole(rol: any) {
+    if (rol == 9) {
+      this.data.ROL = 9;
+      this.data.ROL_NAME = 'Productor';
+      const horaIngreso = new Date();
+      sessionStorage.setItem('user', JSON.stringify(this.data));
+      sessionStorage.setItem('horaIngreso', horaIngreso.toString());
       this.router.navigate(['/productor']);
+    }
+    else if (rol == 10) {
+      this.data.ROL = 10;
+      this.data.ROL_NAME = 'Admin';
+      const horaIngreso = new Date();
+      sessionStorage.setItem('user', JSON.stringify(this.data));
+      sessionStorage.setItem('horaIngreso', horaIngreso.toString());
+      this.router.navigate(['/mantenedor']);
+    }
+    else if (rol == 11) {
+      this.data.ROL = 11;
+      this.data.ROL_NAME = 'Consumidor';
+      const horaIngreso = new Date();
+      sessionStorage.setItem('user', JSON.stringify(this.data));
+      sessionStorage.setItem('horaIngreso', horaIngreso.toString());
+      this.router.navigate(['/consumidor']);
+    }
+    else if (rol == 12) {
+      this.data.ROL = 12;
+      this.data.ROL_NAME = 'Gestor';
+      const horaIngreso = new Date();
+      sessionStorage.setItem('user', JSON.stringify(this.data));
+      sessionStorage.setItem('horaIngreso', horaIngreso.toString());
+      this.router.navigate(['/gestor']);
     }
   }
 }
