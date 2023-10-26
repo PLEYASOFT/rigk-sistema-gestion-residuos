749bee0c174a28af42b684e7b70fe63d7495f506
Patricio Angelini <pangelini@kali>
2023-10-26 15:20:39 -0300

    foo

--- desarrollo/rigk-sg-web/rigk-sg-web/server.js
+++ desarrollo/rigk-sg-web/rigk-sg-web/server.js
@@ -4,14 +4,15 @@ const port = process.env.PORT ||8080;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/rigk-sg-web')));
-
+/*
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname, 'dist/rigk-sg-web/index.html'));
 });
-/*
+*/
+
 app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname, 'dist/rigk-sg-web/.well-known/pki-validation/B0CCF1FEBB5053430E95BD6F3DCB7166.txt'));
-});*/
+	res.sendFile(path.join(__dirname, 'dist/rigk-sg-web/.well-known/pki-validation/9052D92777E046A5033A71FCB9E25E86.txt'));
+});
 
 app.listen(port, () => {
 	console.log('Servidor iniciado');
